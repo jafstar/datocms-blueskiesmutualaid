6d724f439d276e5535829296ada0c1e8086d538a
Jafar Amjad <jafar104@gmail.com>
2023-06-19 11:32:48 -0400

    fix header-logo-link and nav collapse

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,25 @@
 import React from "react"
 import { Container, Navbar, Nav } from "react-bootstrap"
 import Image from 'next/image'
+import Link from "next/link"
+
 
 export default function Header() {
   return (
     <div className={"header"}>
       <Navbar bf={"light"} expand="md">
         <Container>
-          <Navbar.Brand href="/" className='header-logo-link'>
-            <Image
-              className='header-logo-img'
-              src="/images/blueskies-logo.png"
-              width={249}
-              height={240}
-              alt="Blue Skies Mutal Aid"
-            />
-          </Navbar.Brand>
+          <Link href="/">
+            <a className='header-logo-link'>
+              <Image
+                className='header-logo-img'
+                src="/images/blueskies-logo.png"
+                width={249}
+                height={240}
+                alt="Blue Skies Mutal Aid"
+              />
+            </a>
+          </Link>
 
           <Navbar.Toggle />
           <Navbar.Collapse>
